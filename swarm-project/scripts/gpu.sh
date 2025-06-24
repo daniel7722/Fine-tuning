92bfb4c6aaf76d5019a39caef6946c8e5e864d94
Daniel Huang <daniel.huang20@imperial.ac.uk>
2025-06-24 15:38:05 +0100

    feat: added gpu script

---
+++ swarm-project/scripts/gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SBATCH --gres=gpu:1
+# SBATCH --mail-type=ALL # required to send email notifcations
+# SBATCH --mail-user=dh320 # required to send email notifcations - please replace <your_username> with your college login name or email address
+export PATH=/vol/bitbucket/${USER}/tiny/bin/:$PATH
+# the above path could also point to a miniconda install
+# if using miniconda, uncomment the below line
+# source ~/.bashrc
+source activate
+source /vol/cuda/12.0.0/setup.sh
+/usr/bin/nvidia-smi
+uptime
+python3 Fine-tuning/swarm-project/python_sims/model.py
